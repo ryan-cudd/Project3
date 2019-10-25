4c69189f04ee4c47380159210411d859b898b437
Ryan Cudd <ryancudd14@gmail.com>
2019-10-24 20:04:19 -0500

    Added constructor, getSeconds() and getValueOfSeconds() methods

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -1,11 +1,21 @@
 
 public class DateTimeOne extends MesoDateTimeOneAbstract
 {
-
+	
+	private static final long MILLI_CONVERSION = 1000;
+	private long milliSeconds;
+	
+	public DateTimeOne() {
+		milliSeconds = System.currentTimeMillis();
+	}
+	
+	public long getSeconds() {
+		return milliSeconds / MILLI_CONVERSION;
+	}
+	
 	@Override
 	int getValueOfSecond() {
-		// TODO Auto-generated method stub
-		return 0;
+		return (int) (getSeconds() % 60);
 	}
 
 	@Override
